5cb307a637bd7714e8935e9d2888c5100a526bba
Kim Morin <kimmorin93@gmail.com>
2017-11-29 16:05:46 -0500

    MailController update

--- dist/MailController.php
+++ dist/MailController.php
@@ -2,35 +2,24 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+use Mailgun\Mailgun;
 
-$mail = new PHPMailer;
+$mg = Mailgun::create('key-1715c074f053673f6e3c4c79e8595390');
 
 $name = $_POST['name'];
 $email = $_POST['email'];
 $company = $_POST['company'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
-
-$mail->setFrom($email, $name);
-$mail->addAddress('josh@darkroast.co', 'Josh Stobbs');
-$mail->addReplyTo('josh@darkroast.co', 'Josh Stobbs');
-$mail->ReutrnPath='josh@darkroast.co';
-
-$mail->isHTML(true);
-
-$body = 'Name: ' . $name . "\n" .
-        'Comapny: ' . $comapny . "\n" .
-        'Email: ' . $email . "\n" .
-        'Phone: ' . $phone . "\n" .
-        'Message: ' . $message . "\n" . 
-
-$mail->Subject = 'A new contact from' . $name;
-$mail->Body    = $body;
-$mail->AltBody = $body;
-
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message has been sent';
-}
+# Now, compose and send your message.
+# $mg->messages()->send($domain, $params);
+$mg->messages()->send('frassinternational.com', [
+  'from'    => $_POST['email'],
+  'to'      => 'josh@darkroast.co',
+  'subject' => 'A new contact from ' . $name,
+  'html'    => 'Name: ' . $name . "\n" .
+                'Comapny: ' . $comapny . "\n" .
+                'Email: ' . $email . "\n" .
+                'Phone: ' . $phone . "\n" .
+                'Message: ' . $message . "\n"
+]);
